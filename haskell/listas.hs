a90b5b975465f74f8dc3dd617d67c1431615a2d9
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2022-05-16 22:36:41 -0300

    adiciona mais questões

--- haskell/listas.hs
+++ haskell/listas.hs
@@ -16,3 +16,29 @@ compress :: (Eq a) => [a] -> [a]
 compress xs | xs == [] = []
             | elem (last xs) (init xs) = compress (init xs)
             | otherwise = (compress (init xs)) ++ [last xs]
+
+slice :: [a] -> Int -> Int -> [a]
+slice xs imin imax | imax == 1 = [head xs]
+                   | imin == 1 = [head xs] ++ slice (tail xs) imin (imax - 1)
+                   | otherwise = slice (tail xs) (imin - 1) (imax - 1)
+
+insertAt :: a -> Int -> [a] -> [a]
+insertAt el pos xs | pos == 1 = [el] ++ xs
+                   | otherwise = [head xs] ++ insertAt el (pos - 1) (tail xs)
+
+maxList :: (Ord a) => [a] -> a
+maxList xs = foldr max (last xs) (init xs)
+
+buildPalindrome :: (Eq a) => [a] -> [a]
+buildPalindrome xs | xs == [] = []
+                   | otherwise = [head xs] ++ buildPalindrome (tail xs) ++ [head xs]
+
+find :: (Eq a) => (a -> Bool) -> [a] -> a
+find p xs | xs == [] = error "Nenhum elemento satisfaz o predicado"
+          | p (head xs) = head xs
+          | otherwise = find p (tail xs)
+
+count :: (Eq a) => a -> [a] -> Int
+count x [] = 0
+count x xs | x == head xs = 1 + count x (tail xs)
+           | otherwise = count x (tail xs)
