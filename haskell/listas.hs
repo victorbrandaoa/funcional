e5bb3cb9f65ac2dfd5f14a52955397234c17a1f1
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2022-05-21 18:52:50 -0300

    refatora questões lista 2

--- haskell/listas.hs
+++ haskell/listas.hs
@@ -1,44 +1,79 @@
-penultimo :: (Eq a, Ord a) => [a] -> a
-penultimo xs | length xs <= 1 = error "Lista sem penultimo"
-             | length (tail xs) == 1 = head xs
-             | otherwise = penultimo (tail xs)
+penultimo [] = error "Lista sem penultimo"
+penultimo (x:[]) = error "Lista sem penultimo"
+penultimo (x:y:[]) = x
+penultimo (x:xs) = penultimo xs
 
-elementAt :: Int -> [a] -> a
-elementAt i xs | i == 1 = head xs
-               | otherwise = elementAt (i - 1) (tail xs)
+elementAt 1 (x:xs) = x
+elementAt i (x:xs) = elementAt (i - 1) xs
 
-isPalindrome :: (Eq a, Ord a) => [a] -> Bool
-isPalindrome xs | length xs <= 1 = True
-                | (last xs) /= (head xs) = False
-                | otherwise = isPalindrome (tail (init xs))
+isPalindrome [] = True
+isPalindrome (x:[]) = True
+isPalindrome xs | (last xs) /= (head xs) = False
+                | otherwise = isPalindrome (tail (init xs)) 
 
-compress :: (Eq a) => [a] -> [a]
-compress xs | xs == [] = []
-            | elem (last xs) (init xs) = compress (init xs)
+compress [] = []
+compress xs | elem (last xs) (init xs) = compress (init xs)
             | otherwise = (compress (init xs)) ++ [last xs]
 
-slice :: [a] -> Int -> Int -> [a]
-slice xs imin imax | imax == 1 = [head xs]
-                   | imin == 1 = [head xs] ++ slice (tail xs) imin (imax - 1)
-                   | otherwise = slice (tail xs) (imin - 1) (imax - 1)
+removeElement xs e = filter (/=e) xs
 
-insertAt :: a -> Int -> [a] -> [a]
-insertAt el pos xs | pos == 1 = [el] ++ xs
-                   | otherwise = [head xs] ++ insertAt el (pos - 1) (tail xs)
+compact [] = []
+compact (x:xs) = (filter (==x) (x:xs)) ++ compact (removeElement xs x)
 
-maxList :: (Ord a) => [a] -> a
+count xs e = length (filter (==e) xs)
+
+encode [] = []
+encode (x:xs) = (x, count (x:xs) x):encode (removeElement xs x)
+
+split xs i = [take i xs] ++ [drop i xs]
+
+sumFoldl (x:xs) = foldl (+) x xs
+
+length' xs = foldl (\acc _ -> 1 + acc) 0 xs
+
+mean xs = (fromIntegral $ sumFoldl xs) / (fromIntegral $ length' xs)
+
+myAppend [] ys = ys
+myAppend (x:xs) ys = x:(myAppend xs ys)
+
+myAppend' xs ys = foldr (\x acc -> x:acc) ys xs
+
+unique [] = []
+unique (x:xs) = x:unique (removeElement xs x)
+
+unique' xs = foldr (\x acc -> if elem x acc then acc else x:acc) [] xs
+
+myConcat [] = []
+myConcat (x:xs) = x ++ myConcat xs
+
+concatFoldr xs = foldr (++) [] xs
+
+lessThan xs p = filter (<p) xs
+
+greaterThan xs p = filter (>=p) xs
+
+lessThan' xs p = [x | x <- xs, x < p]
+
+greaterThan' xs p = [x | x <- xs, x >= p]
+
+quickSortFilter [] = []
+quickSortFilter (x:xs) = (quickSortFilter (lessThan xs x)) ++ [x] ++ (quickSortFilter (greaterThan xs x))
+
+quickSort [] = []
+quickSort (x:xs) = (quickSort (lessThan' xs x)) ++ [x] ++ (quickSort (greaterThan' xs x))
+
+slice (x:xs) imin 1 = [x]
+slice (x:xs) 1 imax = x:slice xs 1 (imax - 1)
+slice (x:xs) imin imax = slice xs (imin - 1) (imax - 1)
+
+insertAt el 1 xs = el:xs
+insertAt el pos (x:xs) = x:insertAt el (pos -1) xs
+
 maxList xs = foldr max (last xs) (init xs)
 
-buildPalindrome :: (Eq a) => [a] -> [a]
-buildPalindrome xs | xs == [] = []
-                   | otherwise = [head xs] ++ buildPalindrome (tail xs) ++ [head xs]
+buildPalindrome [] = []
+buildPalindrome (x:xs) = [x] ++ buildPalindrome xs ++ [x]
 
-find :: (Eq a) => (a -> Bool) -> [a] -> a
-find p xs | xs == [] = error "Nenhum elemento satisfaz o predicado"
-          | p (head xs) = head xs
-          | otherwise = find p (tail xs)
-
-count :: (Eq a) => a -> [a] -> Int
-count x [] = 0
-count x xs | x == head xs = 1 + count x (tail xs)
-           | otherwise = count x (tail xs)
+find p [] = error "Nenhum elemento satisfaz o predicado"
+find p (x:xs) | p x = x
+              | otherwise = find p xs
